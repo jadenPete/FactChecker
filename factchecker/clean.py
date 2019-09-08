d743f9c5b53473a6d55e6b1d357b513035b87a31
Jaden Peterson <jadenpeterson150@gmail.com>
2019-09-08 14:58:28 -0700

    Clean up the clean script a bit, make it more similar to the crawler

--- factchecker/clean.py
+++ factchecker/clean.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
-os.chdir(os.path.join("input", "huffpost"))
 
-words = ["download", "real", "life", "real", "news", "real",
-         "voices", "help", "us", "tell", "more", "of",
-         "the", "stories", "that", "matter", "from", "voices",
-         "that", "too", "often", "remain", "unheard", "join",
-         "huffpost", "plus"]
+def huffpost():
+	words = ["download", "real", "life", "real", "news", "real",
+	         "voices", "help", "us", "tell", "more", "of",
+	         "the", "stories", "that", "matter", "from", "voices",
+	         "that", "too", "often", "remain", "unheard", "join",
+	         "huffpost", "plus"]
 
-for name in os.listdir():
-	with open(name, "r") as file:
-		article = file.read().splitlines()
+	for name in os.listdir():
+		with open(name, "r") as file:
+			article = file.read().splitlines()
 
-	if article[-len(words):] == words:
-		print(os.path.join("huffpost", name))
+		if article[-len(words):] == words:
+			print(os.path.join("huffpost", name))
 
-		with open(name, "w") as file:
-			file.writelines(word + "\n" for word in article[:-len(words)])
+			with open(name, "w") as file:
+				file.writelines(word + "\n" for word in article[:-len(words)])
 
-os.chdir(os.path.join(os.pardir, "theblaze"))
 
-for name in os.listdir():
-	with open(name, "r") as file:
-		old = file.read()
-		new = old.replace("www\nyoutube\ncom\n", "")
+def theblaze():
+	for name in os.listdir():
+		with open(name, "r") as file:
+			old = file.read()
+			new = old.replace("www\nyoutube\ncom\n", "")
 
-	if new != old:
-		print(os.path.join("theblaze", name))
+		if new != old:
+			print(os.path.join("theblaze", name))
 
-		with open(name, "w") as file:
-			file.write(new)
+			with open(name, "w") as file:
+				file.write(new)
+
+
+{"huffpost": huffpost,
+ "theblaze": theblaze}[sys.argv[1]]()
