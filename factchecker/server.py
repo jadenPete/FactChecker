4a879d8d8c7824b993a67d8e674c342967ad3a6c
Jaden Peterson <jadenpeterson150@gmail.com>
2019-10-06 20:59:42 -0700

    Using Flask for server now

--- factchecker/server.py
+++ factchecker/server.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from __init__ import latest_model, text_to_sequences
+from flask import Flask, request
 import os
+
+app = Flask(__name__)
+tokenizer, model = latest_model()
 
 with open(os.path.join(os.pardir, "selectors.json"), "r") as file:
 	selectors = file.read()
 
 
-class RequestHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		if self.path == "/selectors.json":
-			self.send_response(200)
-			self.end_headers()
-			self.wfile.write(selectors.encode())
-		else:
-			self.send_response(404)
+@app.route("/predict", methods=["POST"])
+def predict():
+	return model.predict(text_to_sequences(tokenizer, request.json))[0][0]
 
 
-HTTPServer(("127.0.0.1", 8080), RequestHandler).serve_forever()
+@app.route("/selectors.json")
+def selectors_json():
+	return selectors.encode()
